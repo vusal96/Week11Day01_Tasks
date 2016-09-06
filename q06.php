0691c9af25b0563ffa9d46b1189d7bee7fec357d
Vusal Abbasov <Vusal Abbasov>
2016-09-06 13:01:40 +0400

    q06 added

--- q06.php
+++ q06.php
@@ -3,5 +3,9 @@
 Qeyd: Uzun hesablamalar aparmaga ehtiyac yoxdur. maksimum 4 setirle hell edin*/
 
 
+$date1=date_create("1996-06-23");
+$date2=date_create("2016-09-06");
+$diff=date_diff($date1,$date2);
+echo $diff->format("%y Year %m Month %d Day");
 
  ?>
